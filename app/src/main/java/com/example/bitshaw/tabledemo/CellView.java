22ef25343018728e3adf9f1284f18135c5cba89a
BITxiao666 <xiaoziyuan@bit.edu.cn>
2018-01-24 23:10:02 +0800

    圆角的Cell

--- app/src/main/java/com/example/bitshaw/tabledemo/CellView.java
+++ app/src/main/java/com/example/bitshaw/tabledemo/CellView.java
@@ -14,25 +14,30 @@ import android.widget.TextView;
  */
 
 public class CellView extends android.support.v7.widget.AppCompatTextView {
-    private int mBgColor = 0; //背景颜色
-    private int mCellSize = 0; //圆角大小
+    private int mBgColor = 0;                   //背景颜色
+    private int coner_size = 0;                     //圆角大小
 
-    public CellView(Context context, int bgColor, int cornerSize) {
+    /**
+     *
+     * @param context
+     * @param bgColor
+     * @param _coner_size 调用时传 dip2px(this, 3)
+     */
+    public CellView(Context context, int bgColor,int _coner_size) {
         super(context);
         mBgColor = bgColor;
-        mCellSize = cornerSize;
+        coner_size = _coner_size;
     }
     @Override
     protected void onDraw(Canvas canvas) {
 
         Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(mBgColor);
+        paint.setAntiAlias(true);               //设置画笔为无锯齿
+        paint.setColor(mBgColor);               //设置画笔颜色
         paint.setAlpha(180);
         paint.setStyle(Paint.Style.FILL);
-        canvas.drawRoundRect(new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight()), mCellSize, mCellSize, paint);
+        canvas.drawRoundRect(new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight()), coner_size, coner_size, paint);
 
         super.onDraw(canvas);
     }
-
 }
--- app/src/main/java/com/example/bitshaw/tabledemo/MainActivity.java
+++ app/src/main/java/com/example/bitshaw/tabledemo/MainActivity.java
@@ -30,11 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.week)
     LinearLayout week;
+
     @BindView(R.id.segments)
     LinearLayout segments;
+
     @BindViews({R.id.weekPanel_1, R.id.weekPanel_2, R.id.weekPanel_3, R.id.weekPanel_4,
             R.id.weekPanel_5})
     List<LinearLayout> mcellViews;
+
+    private int sectionHeight=50;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         fun();
         initDayView();
         initSegmentView();
-        initCell();
+        initCell_1();
     }
 
     public void initDayView() {
@@ -74,7 +78,9 @@ public class MainActivity extends AppCompatActivity {
     private void initSegmentView() {
         int i;
         TextView segment;
-        LinearLayout.LayoutParams one_grid = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout.LayoutParams one_grid = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
         for (i = 0; i < 5; i++) {
             segment = new TextView(this);
             segment.setText("1");
@@ -88,6 +94,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 临时测试用
+     */
     public static Course course1 = new Course("Java基础","金旭亮");
     public static Course course2 = new Course("计算机组成原理","马忠梅");
     public static List<Lesson> lessons = new ArrayList<>();
@@ -96,29 +105,40 @@ public class MainActivity extends AppCompatActivity {
         lessons.add(new Lesson(course2,1,2));
     }
 
+    /**
+     * end
+     */
+
     public void initCell() {
         int lesson_count=0;
         Lesson lesson = lessons.get(0);
         for (int i=1;i<=1;i++) {
             for (int j=1;j<=5;j++) {
                 if(lesson.getWhat_day()==i && lesson.getWhich_segment()==j) {
-                    LinearLayout.LayoutParams one_cell = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-                    TextView textView = new TextView(this);
+                    LinearLayout.LayoutParams one_cell = new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT);
+                    CellView textView = new CellView(this,
+                            GridColor.getCourseBgColor(j),
+                            dip2px(this,5));
                     textView.setText(lesson.get_class_name());
-                    textView.setBackgroundColor(GridColor.getCourseBgColor(j));
+                    //textView.setBackgroundColor(GridColor.getCourseBgColor(j));
                     textView.setTextColor(Color.parseColor("#FFFFFF"));
+                    textView.setTextSize(12);
                     one_cell.gravity = Gravity.CENTER | Gravity.CENTER_HORIZONTAL;
                     one_cell.weight = 1;
                     textView.setGravity(Gravity.CENTER_HORIZONTAL);
                     textView.setLayoutParams(one_cell);
-                    mcellViews.get(0).addView(textView);
+                    mcellViews.get(i-1).addView(textView);
                     lesson_count++;
                     if(lesson_count<lessons.size()) {
                         lesson = lessons.get(lesson_count);
                     }
                 }
                 else {
-                    LinearLayout.LayoutParams one_cell = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                    LinearLayout.LayoutParams one_cell = new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT);
                     TextView textView = new TextView(this);
                     textView.setText("");
                     textView.setBackgroundColor(Color.parseColor("#FFFFFF"));
@@ -127,10 +147,84 @@ public class MainActivity extends AppCompatActivity {
                     one_cell.weight = 1;
                     textView.setGravity(Gravity.CENTER_HORIZONTAL);
                     textView.setLayoutParams(one_cell);
-                    mcellViews.get(0).addView(textView);
+                    mcellViews.get(i-1).addView(textView);
                 }
             }
         }
     }
 
+    public void initCell_1(){
+        int lesson_count=0;
+        Lesson lesson = lessons.get(0);
+        for (int i=1;i<=1;i++) {
+            for (int j=1;j<=5;j++) {
+                if (lesson.getWhat_day() == i && lesson.getWhich_segment() == j) {
+                    LinearLayout linearLayout = new LinearLayout(this);
+                    LinearLayout.LayoutParams Lp = new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT);
+                    Lp.weight = 1;
+                    Lp.gravity = Gravity.CENTER | Gravity.CENTER_HORIZONTAL;
+                    linearLayout.setLayoutParams(Lp);
+                    /**
+                     *    原始代码
+                     */
+                    LinearLayout.LayoutParams one_cell = new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT);
+                    TextView textView = new TextView(this);
+                    textView.setText(lesson.get_class_name());
+                    textView.setBackgroundColor(GridColor.getCourseBgColor(j));
+                    textView.setTextColor(Color.parseColor("#FFFFFF"));
+                    one_cell.gravity = Gravity.CENTER | Gravity.CENTER_HORIZONTAL;
+                    textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                    textView.setLayoutParams(one_cell);
+                    //mcellViews.get(i-1).addView(textView);
+                    lesson_count++;
+                    if(lesson_count<lessons.size()) {
+                        lesson = lessons.get(lesson_count);
+                    }
+                    /**
+                     *   end
+                     */
+                    linearLayout.setPadding(2, 2, 2, 2);
+                    linearLayout.addView(textView);
+                    mcellViews.get(i-1).addView(linearLayout);
+                }
+                else{
+                    LinearLayout linearLayout = new LinearLayout(this);
+                    LinearLayout.LayoutParams Lp = new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT);
+                    Lp.weight = 1;
+                    Lp.gravity = Gravity.CENTER | Gravity.CENTER_HORIZONTAL;
+                    linearLayout.setLayoutParams(Lp);
+                    /**
+                     *    原始代码
+                     */
+                    LinearLayout.LayoutParams one_cell = new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT);
+                    TextView textView = new TextView(this);
+                    textView.setText("");
+                    textView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                    textView.setTextColor(Color.parseColor("#FFFFFF"));
+                    one_cell.gravity = Gravity.CENTER | Gravity.CENTER_HORIZONTAL;
+                    textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                    textView.setLayoutParams(one_cell);
+                    //mcellViews.get(i-1).addView(textView);
+                    /**
+                     *   end
+                     */
+                    linearLayout.addView(textView);
+                    mcellViews.get(i-1).addView(linearLayout);
+                }
+            }
+        }
+    }
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
 }
